e3c2d496ad7e62d42b23eecbd9d14ef5d0992357
mo valipour <valipour@gmail.com>
2015-08-06 00:21:15 +0100

    Add version displaying Re: #12

--- src/commands/index.js
+++ src/commands/index.js
@@ -3,10 +3,36 @@ require('./project');
 require('./init');
 require('./time');
 
+var utils = require('../utils');
 var cli = require('dastoor').builder;
 
+var version = require('../../package.json').version;
+
 var onetime = cli.node('onetime', {
-    help: 'manage harvest and target-proess in one place'
+    help: {
+        description: [
+            'manage harvest and target-proess in one place',
+            '',
+            "                      .-.  _                ",
+            "                     .' `.:_;               ",
+            "     .--. ,-.,-. .--.`. .'.-.,-.,-.,-. .--. ",
+            "    ' .; :: ,. :' '_.': : : :: ,. ,. :' '_.'",
+            "    `.__.':_;:_;`.__.':_; :_;:_;:_;:_;`.__.'",
+            '',
+            '    version ' + version
+        ].join('\n'),
+        options: [{
+            name: '-v, --version', description: 'show onetime version'
+        }]
+    },
+    controller: function (args) {
+        if(args.v || args.version){
+            utils.log();
+            utils.log.chalk('green', '>   ' + version);
+            utils.log();
+        }
+        else return 'h';
+    }
 });
 
 module.exports = onetime;
